a5c98c805a39212a4a670cbef36b6327053549bd
kamipatel <kamipatel@gmail.com>
2019-05-23 14:01:16 -0500

    update

--- orgInit.sh
+++ orgInit.sh
@@ -5,7 +5,7 @@ sfdx force:source:push -f
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 sfdx force:user:permset:assign -n BankPerms
 sfdx force:data:tree:import -f data/export-demo-Recommendations.json
-sfdx force:data:bulk:upsert -s Account -f data/churn.csv -i extId__c --json
+sfdx force:data:bulk:upsert -s Account -f data/cust.csv -i extId__c --json
 sfdx force:org:open
 
 
