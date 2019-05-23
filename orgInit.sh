4e7112816e8c32656891abc52bae490bedec3680
kamipatel <kamipatel@gmail.com>
2019-05-23 13:53:44 -0500

    updates

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sfdx force:org:create -s -f config/project-scratch-def.json -a ADK --setdefaultusername -d 10
-sfdx force:source:push 
+sfdx force:source:push -f
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 sfdx force:user:permset:assign -n BankPerms
 sfdx force:data:tree:import -f data/export-demo-Recommendations.json
-sfdx force:data:bulk:upsert -s Account -f data/Churn.csv -i extId__c
+sfdx force:data:bulk:upsert -s Account -f data/churn.csv -i extId__c --json
 sfdx force:org:open
 
 
